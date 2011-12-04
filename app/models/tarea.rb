88f5b7d266c052e1bc55ee908d7a5f497b67c99b
Rafael Rebolledo <tecnosistemasca@gmail.com>
2011-12-04 13:33:58 -0430

    Agregada algunas validaciones en el modelo

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -1,4 +1,6 @@
 class Tarea < ActiveRecord::Base
-validates_presence_of :nombre
 default_scope :conditions => {:cumplida => false}
+validates :nombre,  :presence => true
+  validates :notas, :presence => true,
+                    :length => { :minimum => 5 }
 end
